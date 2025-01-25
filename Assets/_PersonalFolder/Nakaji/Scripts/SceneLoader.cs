b433dd00fe396b3180460e4bfe8dc5cf53c87f4e
kikuen <naka1028koki@gmail.com>
2025-01-25 18:07:52 +0900

    チュートリアル修正

--- Assets/_PersonalFolder/Nakaji/Scripts/SceneLoader.cs
+++ Assets/_PersonalFolder/Nakaji/Scripts/SceneLoader.cs
@@ -5,8 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    public string nextSceneName;
    public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+    private void Update()
+    {
+        if (Input.anyKey && !Input.GetMouseButton(0) && !Input.GetMouseButton(1) && !Input.GetMouseButton(2))
+        {
+            LoadScene(nextSceneName);
+        }
     }
 }
